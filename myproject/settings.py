c4921434b4ffd4715cd3785aa839380069ad361b
Mauro Castillo III <mauro.m.castillo3@gmail.com>
2024-07-19 01:36:56 -0500

    Added allowed hosts.

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-cipdcv%z0kwinuu&o7o3d5-tm+-_0joohal-8a34nj)*ogdxst
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.145.150.247', 'localhost', '127.0.0.1']
 
 
 # Application definition
